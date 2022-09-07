64024d42f41847bc50b622fc7d183d5429d29ca4
KelvinQiu <kelvinqiu802@outlook.com>
2022-09-07 22:18:05 +0800

    feat: default arr

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Visualization from './components/Visualization';
 
 function App() {
   const [formData, setFormData] = React.useState({
-    arrStr: '1,2,3,4,5,6,7,8,9',
+    arrStr: '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20',
     target: '2',
   });
   const [start, setStart] = React.useState(false);
