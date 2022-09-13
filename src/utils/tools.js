107f6f03c631e05337517f04d34fefe953205453
KelvinQiu <kelvinqiu802@outlook.com>
2022-09-13 20:35:43 +0800

    feat: offset calc function

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,3 +1,9 @@
 export function strToArr(arrStr) {
   return arrStr.split(',');
 }
+
+export function getOffsetByIndex(index) {
+  const elem = document.getElementById(index);
+  const offset = elem.offsetLeft;
+  return offset;
+}
